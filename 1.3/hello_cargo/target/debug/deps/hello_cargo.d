2a33fd06ff0f8f2abbeb4e48ee7910f4d49baf2d
Brendan John <bjohn454@gmail.com>
2021-03-29 16:46:45 -0400

    remove compiled files

--- 1.3/hello_cargo/target/debug/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-/Users/bjohn/Repos/rust-tutorial/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
-
-/Users/bjohn/Repos/rust-tutorial/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
-
-src/main.rs:
--- 1.3/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/bjohn/Repos/rust-tutorial/1.3/target/debug/hello_cargo: /Users/bjohn/Repos/rust-tutorial/1.3/src/main.rs
