3775fe3c546ecced51328e553548a5d657cc1d72
Brendan John <bjohn454@gmail.com>
2021-04-01 16:56:24 -0400

    oninit greet()

--- hello-wasm/ng-wasm/src/app/app.component.ts
+++ hello-wasm/ng-wasm/src/app/app.component.ts
@@ -1,20 +1,20 @@
-import { AfterViewChecked, AfterViewInit, Component } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewChecked {
+export class AppComponent implements OnInit {
   title = 'ng-wasm';
   constructor() {
 
   }
 
-  ngAfterViewChecked() {
-    import('ng-wasm').then(({greet})=>{
+  ngOnInit() {
+    import('ng-wasm').then(({ greet }) => {
       greet();
-    })
-
+    });
   }
+
 }
