5c9d6cee14eb46713310acdc647ca828a343b2d5
Brendan John <bjohn454@gmail.com>
2021-04-01 16:55:10 -0400

    wasm import sorta working in ng

---
+++ hello-wasm/ng-wasm/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { AfterViewChecked, AfterViewInit, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements AfterViewChecked {
+  title = 'ng-wasm';
+  constructor() {
+
+  }
+
+  ngAfterViewChecked() {
+    import('ng-wasm').then(({greet})=>{
+      greet();
+    })
+
+  }
+}
