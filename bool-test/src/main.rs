04396f7e2fa1c840bef794bd20efb3c5a7e96d6a
Brendan John <bjohn454@gmail.com>
2021-04-17 20:42:56 -0400

    dumb bool test

---
+++ bool-test/src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let weird_bool = do_iter();
+    let ret = match weird_bool {
+        true => 10,
+        false => 20,
+    };
+    println!("The ret is {}", ret);
+}
+
+// fn get_bool() -> bool {
+//     let ret = true;
+//     return ret;
+// }
+
+fn do_iter() -> bool {
+    let comparer = 7;
+    let arr = vec![4, 6, 8];
+    let mut iter = arr.iter();
+    let mut cur = iter.next();
+    let mut joined = false;
+
+    while !cur.is_none() {
+        if cur == Some(&comparer) {
+            joined = true;
+        }
+        cur = iter.next();
+    }
+    joined
+}
